b0fdc4032691228761b2306da05cfe08b70887ff
ObayaDevOps <obayajamesdralega@gmail.com>
2021-09-15 13:17:40 +0300

    centred red square

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Sketch from 'react-p5';
 
-
 function App() {
-  // eslint-disable-next-line
-  let a = 300;
-  // eslint-disable-next-line
-  let b = 300;
+
   let setup = (p5, canvasParentRef) => {
     // eslint-disable-next-line
     let myCanvas = p5.createCanvas(1000, 800).parent(canvasParentRef);
 
+    
     //centre the sketch
      let x = (p5.windowWidth - p5.width) / 2;
      let y = (p5.windowHeight - p5.height) / 2;
