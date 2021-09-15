c2717b42fc0b85669c10aa800d7107774cfddf29
ObayaDevOps <obayajamesdralega@gmail.com>
2021-09-15 13:54:45 +0300

    horizontally moving ball

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Sketch from 'react-p5';
 
 function App() {
+  let a = 300;
+  let b = 300;
+  let speed = 3;
 
   let setup = (p5, canvasParentRef) => {
     // eslint-disable-next-line
@@ -15,8 +18,27 @@ function App() {
 
   };
 
+  //To animate an object, we add conditional statements to the draw function
   let draw = (p5) => {
     p5.background("rgb(100%,0%,10%)");
+
+    //the ball
+    p5.stroke(255);
+    p5.strokeWeight(4);
+    p5.noFill(); //would be cool to make it multicoloured
+
+    //(a,b) Positioning, last 2 params are for size
+    p5.ellipse(a, b, 100, 100);
+
+    if (a >= p5.width) {
+      speed = -3;
+    }
+    if(a === 90) {
+      speed = 3;
+    }
+
+    a = a + speed;
+
   };
 
   return (
